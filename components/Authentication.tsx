28930a6426e012c8fe310887f0705b78f8564682
Ronald Martin <hello@itsronald.com>
2021-08-21 15:28:15 -0700

    Format with prettier

--- components/Authentication.tsx
+++ components/Authentication.tsx
@@ -1,25 +1,28 @@
 import * as React from "react";
 import { Session } from "../session/session";
-import { SessionUserContext, SessionUserContextValue } from "../session/useSessionUser";
+import {
+  SessionUserContext,
+  SessionUserContextValue,
+} from "../session/useSessionUser";
 
 interface AuthenticationProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
 export function Authentication({ children }: AuthenticationProps) {
-    const [session, setSession] = React.useState<Session>(null);
+  const [session, setSession] = React.useState<Session>(null);
 
-    function login(session: Session) {
-        setSession(session);
-    }
+  function login(session: Session) {
+    setSession(session);
+  }
 
-    function logout() {
-        setSession(null);
-    }
+  function logout() {
+    setSession(null);
+  }
 
-    return (
-        <SessionUserContext.Provider value={{ session, login, logout }}>
-            {children}
-        </SessionUserContext.Provider>
-    );
+  return (
+    <SessionUserContext.Provider value={{ session, login, logout }}>
+      {children}
+    </SessionUserContext.Provider>
+  );
 }
