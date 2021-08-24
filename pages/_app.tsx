a2146158ee38e1fb9e7674a8fc3fb199b1842098
Ronald Martin <hello@itsronald.com>
2021-08-23 23:16:08 -0700

    Add more menu options for logged in user

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import {
   MenuList,
   Text,
   useDisclosure,
+  MenuGroup,
 } from "@chakra-ui/react";
 import "../styles/globals.css";
 import { theme } from "../styles/theme";
@@ -79,7 +80,15 @@ function AppHeader() {
           </MenuButton>
           <MenuList colorScheme="teal">
             {session == null && <MenuItem onClick={onOpen}>Login</MenuItem>}
-            {session != null && <MenuItem onClick={logout}>Logout</MenuItem>}
+            {session != null && (
+              <>
+                <MenuItem>My Sightings</MenuItem>
+                <MenuGroup>
+                  <MenuItem>Profile</MenuItem>
+                  <MenuItem onClick={logout}>Logout</MenuItem>
+                </MenuGroup>
+              </>
+            )}
           </MenuList>
         </Menu>
       </Flex>
