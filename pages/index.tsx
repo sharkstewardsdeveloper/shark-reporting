3e9bc0d7c04ad5c27e3398e277cf8efc76ac4bc8
Tanishq Kancharla <tklightningfast@gmail.com>
2021-08-25 21:46:38 -0400

    Added form presentation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,40 @@
 import React from "react";
-import { 
+import {
   Container,
   Heading,
   Flex,
   Text,
   Button,
+  Link,
   Stack,
 } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    
-      <Flex 
-        align="center"
-        justify="center"
-        flexDirection="column"
-        width="100%"
-        height="100%"
-        backgroundColor={["brand.primaryLight", "brand.primaryLight", "brand.primaryLight"]}
-        color={["brand.white", "brand.white", "brand.white"]}
-      >
-        <Heading m={1}>Help keep our oceans alive</Heading>
-        <Text>Report shark sightings</Text>
+    <Flex
+      align="center"
+      justify="center"
+      flexDirection="column"
+      width="100%"
+      height="100%"
+      backgroundColor={[
+        "brand.primaryLight",
+        "brand.primaryLight",
+        "brand.primaryLight",
+      ]}
+      color={["brand.white", "brand.white", "brand.white"]}
+    >
+      <Heading m={1}>Help keep our oceans alive</Heading>
+      <Text>Report shark sightings</Text>
 
-        <Stack direction="row" spacing={4} m={3}>
-          <Button colorScheme="teal" variant="solid">
-            Educate
-          </Button>
-          <Button colorScheme="teal" variant="outline">
-            Report
-          </Button>
-        </Stack>
-      </Flex>
-      
-    
+      <Stack direction="row" spacing={4} m={3}>
+        <Button colorScheme="teal" variant="solid">
+          Educate
+        </Button>
+        <Button colorScheme="teal" variant="outline">
+          <Link href="/report">Report</Link>
+        </Button>
+      </Stack>
+    </Flex>
   );
 }
